9b9dceb8bcdcf357c011938ed9df675f223ae8ba
Esin <nbusra.esin@gmail.com>
2023-04-03 16:18:21 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+
+function saturdayFun(action = "roller-skate"){
+    return "This Saturday, I want to " + action + "!" 
+}
+
+
+
+
+function mondayWork(argument = "office"){
+    if (argument != "office" ){
+        return "This Monday, I will work from home."
+    } else { 
+        return "This Monday, I will go to the office."
+    }
+}
+
+
+function wrapAdjective(parOuter = "*" ){
+    return function(parInner = "special"){
+        return "You are " + parOuter + parInner + parOuter +"!"
+    }
+}
+
+// console.log(wrapAdjective("%")("a dedicated programmer"))
